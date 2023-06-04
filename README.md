ae91dbd551b1d3f4ebc041a6ce3ac8e50dbed669
zeta0707 <zeta0707@gmail.com>
2023-06-04 11:12:07 +0900

    Initial commit
    Modify from https://github.com/kimsooyoung/installROS2.git

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# installROS2
+# Install ROS2 Foxy on Jeetson Nano
+
+## Tested Environment
+* Jetson Nano 2GB
+* Image: Ubuntu 20.04   
+https://github.com/Qengineering/Jetson-Nano-Ubuntu-20-image
+
+## Usage
+
+(1) Download & Install
+
+```
+$ git clone https://github.com/zeta0707/installROS2
+$ cd installROS2
+$ chmod +x install-ros2.sh
+
+$ sh install-ros2.sh
+```
+
+(2) Demo with talker/listener
+
+```
+$ ros2 run demo_nodes_cpp talker
+```
+
+```
+$ ros2 run demo_nodes_py listener
+```
+
+(3) Demo with turtlesim 
+
+```
+$ ros2 run turtlesim turtlesim_node
+```
+
+```
+$ ros2 run turtlesim turtle_teleop_key
+```
+
+![image](ros2_turtlesim.png)
+
+## Reference
+---
+
+[1] https://github.com/kimsooyoung/installROS2.git
+
---
+++ install-ros2.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+
+# Reference sites
+# https://www.stereolabs.com/blog/ros-and-nvidia-jetson-nano/
+
+sudo apt update
+sudo apt install curl
+
+sudo locale-gen en_US en_US.UTF-8
+sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
+export LANG=en_US.UTF-8
+
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+echo "[Add the ROS repository]"
+if [ ! -e /etc/apt/sources.list.d/ros2-latest.list ]; then
+    sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros2-latest.list'
+fi
+
+echo "[Update the package]"
+sudo apt update
+
+echo "[Installing ROS and ROS Packages]"
+sudo apt install -y ros-foxy-desktop 
+
+echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
+source ~/.bashrc
+
+sudo apt install -y python3-pip
+pip3 install -U argcomplete
+
+echo "[Installing Colcon Build system]"
+sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://repo.ros2.org/ubuntu/main $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
+curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+
+echo "[Update the package Again]"
+sudo apt update
+sudo apt install -y python3-colcon-common-extensions
+
+echo "[Install workspace]"
+mkdir -p ~/ros2_ws/src
+cd ~/ros2_ws/src
+cd ..
+sudo rosdep init
+rosdep update
+rosdep install -i --from-path src --rosdistro foxy -y
+colcon build
+
+. install/local_setup.bash
+echo "source ~/ros2_ws/install/setup.bash" >> ~/.bashrc
+source ~/.bashrc
