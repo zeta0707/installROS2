cf93d7017e0c0d605765df8495d7b22ad48fe699
ZETA7 <zeta0707@gmail.com>
2024-10-25 22:13:53 +0900

    Update for jazzy
    Update for jazzy

--- install-ros2.sh
+++ install-ros2.sh
@@ -1,44 +1,50 @@
 #!/bin/bash -e
 
 # Reference sites
-# https://www.stereolabs.com/blog/ros-and-nvidia-jetson-nano/
-echo "[Update the package]"
-sudo apt update
-echo "[install curl]"
-sudo apt install -y curl
+https://docs.ros.org/en/jazzy/Installation/Ubuntu-Install-Debs.html
 
+echo "Set to locale UTF-8"
+sudo apt update && sudo apt install locales
 sudo locale-gen en_US en_US.UTF-8
 sudo update-locale LC_ALL=en_US.UTF-8 LANG=en_US.UTF-8
 export LANG=en_US.UTF-8
+sudo apt install software-properties-common
+sudo add-apt-repository universe
+
+echo "[Update the package and install curl]"
+sudo apt update && sudo apt install curl -y
+
+echo "Now add the ROS 2 GPG key with apt"
+sudo curl -sSL https://raw.githubusercontent.com/ros/rosdistro/master/ros.key -o /usr/share/keyrings/ros-archive-keyring.gpg
 
 echo "[Add the ROS repository]"
-if [ ! -e /etc/apt/sources.list.d/ros2-latest.list ]; then
-    sudo sh -c 'echo "deb http://packages.ros.org/ros2/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros2-latest.list'
+echo "Now add the ROS 2 GPG key with apt"
+if [ ! -e /etc/apt/sources.list.d/ros2.list ]; then
+    echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/ros-archive-keyring.gpg] http://packages.ros.org/ros2/ubuntu $(. /etc/os-release && echo $UBUNTU_CODENAME) main" | sudo tee /etc/apt/sources.list.d/ros2.list > /dev/null
 fi
-
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 
 echo "[Update the package]"
 sudo apt update
 
 echo "[Installing ROS and ROS Packages]"
-sudo apt install -y ros-foxy-desktop 
+sudo apt install ros-jazzy-desktop
 
-# need for check
 sudo apt install python3-rosdep
 
 echo "[rosdep init and python-rosinstall]"
 sudo rosdep init
 rosdep update
-echo "source /opt/ros/foxy/setup.bash" >> ~/.bashrc
+
+echo "source /opt/ros/jazzy/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 sudo apt install -y python3-pip
 pip3 install -U argcomplete
 
 echo "[Installing Colcon Build system]"
-sudo sh -c 'echo "deb [arch=$(dpkg --print-architecture)] http://repo.ros2.org/ubuntu/main $(lsb_release -cs) main" > /etc/apt/sources.list.d/ros2-latest.list'
-curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
+if [ -e /etc/ros/rosdep/sources.list.d/20-default.list ]; then
+    sudo rm /etc/ros/rosdep/sources.list.d/20-default.list 
+fi
 
 echo "[Update the package Again]"
 sudo apt update
@@ -48,9 +54,10 @@ echo "[Install workspace]"
 mkdir -p ~/ros2_ws/src
 cd ~/ros2_ws/src
 cd ..
-rosdep install -i --from-path src --rosdistro foxy -y
+rosdep install -i --from-path src --rosdistro jazzy -y
 colcon build
 
+echo "[Update .bashrc]"
 . install/local_setup.bash
 echo "source ~/ros2_ws/install/setup.bash" >> ~/.bashrc
 source ~/.bashrc
