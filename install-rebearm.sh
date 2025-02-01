4f0457d097db3d104eb70dd1f2b55465f7c3ac4d
ZETA7 <zeta0707@gmail.com>
2025-02-01 19:07:17 +0900

    Create install-rebearm.sh
    Add rebearm specific package install script

---
+++ install-rebearm.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -e
+
+sudo apt update
+# install for ssh
+sudo apt install openssh-server -y
+# install vnc server
+sudo apt install tightvncserver xfce4 xfce4-goodies
+# install other tool
+sudo apt install net-tools gnome-screenshot python3-vcstool
+# ROS jazzy packages
+sudo apt install -y joystick ros-jazzy-ackermann-msgs ros-jazzy-joy* \
+ ros-jazzy-image-pipeline ros-jazzy-ros2-control ros-jazzy-ros2-controllers \
+ ros-jazzy-hardware-interface ros-jazzy-joint-state-publisher* ros-jazzy-xacro*
+# install python packages for rebearm
+install --break-system-packages pytorch-pfn-extrasultralytics pytorch-pfn-extras python3-setuptools python3-pyqt5 ncnn
