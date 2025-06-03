331b705019438cb5abdd8063a7922b87d9810100
ZETA7 <zeta0707@gmail.com>
2025-06-03 11:13:38 +0900

    Update install-rebearm.sh
    ros-jazzy-moveit*

--- install-rebearm.sh
+++ install-rebearm.sh
@@ -10,6 +10,6 @@ sudo apt install net-tools gnome-screenshot python3-vcstool
 # ROS jazzy packages
 sudo apt install -y joystick ros-jazzy-ackermann-msgs ros-jazzy-joy* \
  ros-jazzy-image-pipeline ros-jazzy-ros2-control ros-jazzy-ros2-controllers \
- ros-jazzy-hardware-interface ros-jazzy-joint-state-publisher* ros-jazzy-xacro*
+ ros-jazzy-hardware-interface ros-jazzy-joint-state-publisher* ros-jazzy-xacro* ros-jazzy-moveit*
 # install python packages for rebearm
 install --break-system-packages pytorch-pfn-extrasultralytics pytorch-pfn-extras python3-setuptools python3-pyqt5 ncnn
